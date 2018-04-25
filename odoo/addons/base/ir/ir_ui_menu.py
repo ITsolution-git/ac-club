2084eb54357c901c3057701cd115a6cf4bb42413
freshgreen <roy.smith0820@gmail.com>
2018-04-24 22:57:53 -0500

    Hide menus for parent

--- odoo/addons/base/ir/ir_ui_menu.py
+++ odoo/addons/base/ir/ir_ui_menu.py
@@ -115,22 +115,21 @@ class IrUiMenu(models.Model):
             'ir.actions.server': 'model_id',
         }
         
-        namelist = ['Dashboard', 'Discuss', 'Sales', 'Point of Sale', 'Invoicing', 'Inventory'] 
-        print namelist
+        namelist = ['Dashboard', 'Discuss', 'Sales', 'Point of Sale', 'Invoicing', 'Invoices'] 
         for menu in action_menus:
             fname = model_fname.get(menu.action._name)
-
-            if((self.env.user.id != 1) and (menu.name in namelist)):
-                print menu.name
-                continue
 
             if not fname or not menu.action[fname] or \
                     access.check(menu.action[fname], 'read', False):
                 # make menu visible, and its folder ancestors, too
 
+                if((self.env.user.id == 1) or (menu.name not in namelist)):
+                    visible += menu
+
                 menu = menu.parent_id
                 while menu and menu in folder_menus and menu not in visible:
-                    visible += menu
+                    if((self.env.user.id == 1) or (menu.name not in namelist)):
+                        visible += menu
                     menu = menu.parent_id
 
         return set(visible.ids)
