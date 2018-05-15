2b9e14dccb61f10d9219947bf3300ef4d06a2ede
freshgreen <roy.smith0820@gmail.com>
2018-05-14 21:04:56 -0500

    rollback roy's change

--- odoo/addons/base/ir/ir_ui_menu.py
+++ odoo/addons/base/ir/ir_ui_menu.py
@@ -115,7 +115,7 @@ class IrUiMenu(models.Model):
             'ir.actions.server': 'model_id',
         }
         
-        namelist = ['Dashboard', 'Dashboards', 'Discuss', 'Sales', 'Point of Sale', 'Invoicing', 'Invoices'] 
+        namelist = ['Dashboard', 'Dashboards', 'Discuss', 'Sales'] 
         for menu in action_menus:
             fname = model_fname.get(menu.action._name)
 
