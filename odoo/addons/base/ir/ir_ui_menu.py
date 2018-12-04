965fd85494bacb44ff4bce5231d9cfa0ac80e2e7
Itsolution-git <roy.smith0820@gmail.com>
2018-12-04 02:49:04 -0500

    Fix issues

--- odoo/addons/base/ir/ir_ui_menu.py
+++ odoo/addons/base/ir/ir_ui_menu.py
@@ -115,7 +115,7 @@ class IrUiMenu(models.Model):
             'ir.actions.server': 'model_id',
         }
         
-        namelist = ["Sales"] 
+        namelist = []
         for menu in action_menus:
             fname = model_fname.get(menu.action._name)
 
