6ab4a1e3e48635ccc53a01a5e96e5406eaaa13ea
It solution <roy.smith0820@gmail.com>
2019-03-02 12:47:16 -0500

    permission menu hide/show; update logo; change tax info;

--- odoo/addons/base/ir/ir_ui_menu.py
+++ odoo/addons/base/ir/ir_ui_menu.py
@@ -273,6 +273,39 @@ class IrUiMenu(models.Model):
         """
         fields = ['name', 'sequence', 'parent_id', 'action', 'web_icon', 'web_icon_data']
         menu_roots = self.get_user_roots()
+
+        current_uid = self._context.get('uid')
+        user = self.env['res.users'].browse(current_uid)
+
+        if (len(user.employee_ids) == 1):
+            if (user.employee_ids[0].job_id is not None):
+                if (user.employee_ids[0].job_id.name == 'Hotel Manager'):
+                    menu_roots = menu_roots
+                    newIds = []
+
+                    for menu in menu_roots:
+                        if menu.name not in ['Apps']:
+                            newIds.append(menu.id)
+                    menu_roots =  self.search([('id', 'in', newIds)])
+                if (user.employee_ids[0].job_id.name == 'Hotel Staff'):
+                    menu_roots = menu_roots
+                    newIds = []
+
+                    for menu in menu_roots:
+                        if menu.name not in ['Apps', 'Point of Sale', 'Sales']:
+                            newIds.append(menu.id)
+                    menu_roots =  self.search([('id', 'in', newIds)])
+
+                if (user.employee_ids[0].job_id.name == 'POS Staff'):
+                    menu_roots = menu_roots
+                    newIds = []
+
+                    for menu in menu_roots:
+                        if menu.name not in ['Apps', 'Hotel Management', 'Sales', 'Invoicing', 'Settings']:
+                            newIds.append(menu.id)
+                    menu_roots =  self.search([('id', 'in', newIds)])
+                    
+        
         menu_roots_data = menu_roots.read(fields) if menu_roots else []
         menu_root = {
             'id': False,
